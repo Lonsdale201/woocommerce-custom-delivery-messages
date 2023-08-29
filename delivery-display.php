4bb53a5c6821452e7a794718ce019a1fc330a4fc
Soczó <lonsdale201@hotmail.com>
2023-08-30 01:03:46 +0200

    Update delivery-display.php v2.0

--- delivery-display.php
+++ delivery-display.php
@@ -11,104 +11,118 @@ if (!class_exists('Woo_Delivery_Display')) {
     class Woo_Delivery_Display {
 
         public function __construct() {
-            add_action('woocommerce_after_add_to_cart_form', array($this, 'display_notification_message'));
+            $this->notification_placement = get_option('wc_delivery_notification_placement', 'below_add_to_cart');
+        
+            if ($this->notification_placement === 'above_add_to_cart') {
+                add_action('woocommerce_before_add_to_cart_form', array($this, 'display_notification_message'));
+            } elseif ($this->notification_placement === 'below_add_to_cart') {
+                add_action('woocommerce_after_add_to_cart_form', array($this, 'display_notification_message'));
+            }
+        
             add_shortcode('woo_delivery_notification', array($this, 'delivery_notification_shortcode'));
             add_action('woocommerce_before_cart', array($this, 'display_cart_notification'));
         }
         
-        // Megjelenítjük az értesítést, ha a termék a kiválasztottak között van
         public function display_notification_message() {
-            // Beállítások lekérdezése
-            $selected_products = get_option('wc_delivery_selected_products');
+            global $product;
+            $current_product_id = $product->get_id();
+        
+            $selected_products = get_option('wc_delivery_selected_products', []);
+            $selected_categories = get_option('wc_delivery_selected_categories', []);
+        
+            if (in_array($current_product_id, $selected_products) || $this->is_product_in_selected_categories($product, $selected_categories)) {
+                $this->display_selected_product_notification($current_product_id);
+            } else {
+                $this->display_regular_product_notification($current_product_id);
+            }
+        }
+        
+        protected function is_product_in_selected_categories($product, $selected_categories) {
+            $product_categories = $product->get_category_ids();
+            return array_intersect($product_categories, $selected_categories);
+        }
+    
+        protected function display_selected_product_notification($product_id) {
             $notification_title = get_option('wc_delivery_notification_title');
             $notification_message = get_option('wc_delivery_notification_message');
-            $hide_if_out_of_stock = get_option('wc_delivery_hide_if_out_of_stock') === 'yes';
-            $notification_placement = get_option('wc_delivery_notification_placement', 'below_add_to_cart');
-
-            $enable_regular_notification = get_option('wc_delivery_enable_regular_notification') === 'yes';
+    
+            if (empty($notification_message)) {
+                return;
+            }
+    
+            global $product;
+            if (get_option('wc_delivery_hide_if_out_of_stock') === 'yes' && !$product->is_in_stock()) {
+                return;
+            }
+    
+            echo '<div class="woo-delivery-notification-wrapper">';
+            if (!empty($notification_title)) {
+                echo '<div class="woo-delivery-title">' . esc_html($notification_title) . '</div>';
+            }
+            echo '<span class="woo-delivery-message">' . esc_html($notification_message) . '</span>';
+            echo '</div>';
+        }
+    
+        protected function display_regular_product_notification($product_id) {
+            if (get_option('wc_delivery_enable_regular_notification') !== 'yes') {
+                return;
+            }
+        
             $regular_notification_title = get_option('wc_delivery_regular_notification_title');
             $regular_notification_message = get_option('wc_delivery_regular_notification_message');
         
-
-            // Ha nincsenek kiválasztva termékek vagy a szöveg üres, akkor off
-            if (empty($selected_products) || empty($notification_message)) {
+            if (empty($regular_notification_message)) {
                 return;
-            }
-
-            if ($notification_placement === 'shortcode') {
-                return;
-            }
-
-            // Aktuális termék ID-jének lekérdezése
-            global $product;
-            $current_product_id = $product->get_id();
-
-            // Ha az új beállítás engedélyezve van és a termék nem a kiválasztottak között van
-            if ($enable_regular_notification && !in_array($current_product_id, $selected_products)) {
-                if ($hide_if_out_of_stock && !$product->is_in_stock()) {
-                    return;
-                }
-                echo '<div class="woo-delivery-notification-regular-wrapper">';
-
-                // Cím megjelenítése, ha van beállítva
-                if (!empty($regular_notification_title)) {
-                    echo '<div class="woo-delivery-title-regular">' . esc_html($regular_notification_title) . '</div>';
-                }
-
-                echo '<span class="woo-delivery-message-regular">' . esc_html($regular_notification_message) . '</span>';
-                echo '</div>';
             }
         
-
-            // Ha az aktuális termék azonosítója a kiválasztott termékek között van
-            if (in_array($current_product_id, $selected_products)) {
-                // Ha be van kapcsolva a "Hide if out of stock" beállítás és a termék nincs készleten, akkor off
-                if ($hide_if_out_of_stock && !$product->is_in_stock()) {
-                    return;
-                }
-
-                echo '<div class="woo-delivery-notification-wrapper">';
-
-                // Cím megjelenítése, ha van beállítva
-                if (!empty($notification_title)) {
-                    echo '<div class="woo-delivery-title">' . esc_html($notification_title) . '</div>';
-                }
-
-                echo '<span class="woo-delivery-message">' . esc_html($notification_message) . '</span>';
-                echo '</div>';
+            global $product;
+            if (get_option('wc_delivery_hide_if_out_of_stock') === 'yes' && !$product->is_in_stock()) {
+                return;
             }
+        
+            echo '<div class="woo-delivery-notification-regular-wrapper">';
+            if (!empty($regular_notification_title)) {
+                echo '<div class="woo-delivery-title-regular">' . esc_html($regular_notification_title) . '</div>';
+            }
+            echo '<span class="woo-delivery-message-regular">' . esc_html($regular_notification_message) . '</span>';
+            echo '</div>';
         }
+    
+    
+
+        /*
+        //=============================================================================
+        // ** Elhelyezés: Manuális (SHORTCODE) [woo_delivery_notification]
+        //=============================================================================
+        */	
+
+
         public function delivery_notification_shortcode() {
-            // Beállítások lekérdezése
-            $selected_products = get_option('wc_delivery_selected_products');
-            $notification_title = get_option('wc_delivery_notification_title');
-            $notification_message = get_option('wc_delivery_notification_message');
-            $hide_if_out_of_stock = get_option('wc_delivery_hide_if_out_of_stock') === 'yes';
-            $notification_placement = get_option('wc_delivery_notification_placement', 'below_add_to_cart');
-        
-            // Regular beállítások lekérdezése
-            $enable_regular_notification = get_option('wc_delivery_enable_regular_notification') === 'yes';
-            $regular_notification_message = get_option('wc_delivery_regular_notification_message');
-            $regular_notification_title = get_option('wc_delivery_regular_notification_title'); 
-        
-            // Ha a beállítás nem a shortcode-ra van állítva, akkor off
-            if ($notification_placement !== 'shortcode') {
-                return '';
-            }
-        
-            // Aktuális termék ID-jének lekérdezése
             global $product;
+        
+            // Ha nincs termék, ne jelenjen meg semmi
             if (!$product) {
                 return '';
             }
         
             $current_product_id = $product->get_id();
             
-            $output = '';  // Initialize the output variable here
+            $selected_products = get_option('wc_delivery_selected_products', []);
+            $selected_categories = get_option('wc_delivery_selected_categories', []);
         
-            // Ha az aktuális termék azonosítója a kiválasztott termékek között van
-            if (in_array($current_product_id, $selected_products)) {
-                // Ha be van kapcsolva a "Hide if out of stock" beállítás és a termék nincs készleten, akkor off
+            $notification_title = get_option('wc_delivery_notification_title');
+            $notification_message = get_option('wc_delivery_notification_message');
+            $hide_if_out_of_stock = get_option('wc_delivery_hide_if_out_of_stock') === 'yes';
+        
+            // Regular beállítások lekérdezése
+            $enable_regular_notification = get_option('wc_delivery_enable_regular_notification') === 'yes';
+            $regular_notification_message = get_option('wc_delivery_regular_notification_message');
+            $regular_notification_title = get_option('wc_delivery_regular_notification_title'); 
+        
+            $output = '';
+        
+            // Ha az aktuális termék azonosítója a kiválasztott termékek között van vagy a termék kiválasztott kategóriában szerepel
+            if (in_array($current_product_id, $selected_products) || $this->is_product_in_selected_categories($product, $selected_categories)) {
                 if ($hide_if_out_of_stock && !$product->is_in_stock()) {
                     return '';
                 }
@@ -118,9 +132,7 @@ if (!class_exists('Woo_Delivery_Display')) {
                     $output .= '<div class="woo-delivery-title">' . esc_html($notification_title) . '</div>';
                 }
                 $output .= '<span class="woo-delivery-message">' . esc_html($notification_message) . '</span></div>';
-        
             } elseif ($enable_regular_notification) {
-                // Ha be van kapcsolva a "Hide if out of stock" beállítás és a termék nincs készleten, akkor off
                 if ($hide_if_out_of_stock && !$product->is_in_stock()) {
                     return '';
                 }
@@ -136,6 +148,13 @@ if (!class_exists('Woo_Delivery_Display')) {
         }
         
         
+        /*
+        //=============================================================================
+        // ** Kosár oldal
+        //=============================================================================
+        */	
+
+        
         public function display_cart_notification() {
             $cart_reminder = get_option('wc_delivery_cart_reminder', 'no');
             
@@ -144,19 +163,23 @@ if (!class_exists('Woo_Delivery_Display')) {
                 return;
             }
             
-            $selected_products = get_option('wc_delivery_selected_products');
+            $selected_products = get_option('wc_delivery_selected_products', []);
+            $selected_categories = get_option('wc_delivery_selected_categories', []);
             $cart_reminder_message = get_option('wc_delivery_cart_reminder_message');
             
-            if (empty($selected_products) || empty($cart_reminder_message)) {
+            if (empty($cart_reminder_message)) {
                 return;
             }
             
             $matching_products = [];
             
-            // Ellenőrizzük, hogy a kosárban van-e olyan termék, ami a kiválasztott termékek között van
+            // Ellenőrizzük, hogy a kosárban van-e olyan termék, ami a kiválasztott termékek vagy kiválasztott kategóriák között van
             $cart_items = WC()->cart->get_cart();
             foreach ($cart_items as $item) {
-                if (in_array($item['product_id'], $selected_products)) {
+                $product = wc_get_product($item['product_id']);
+                $product_categories = $product->get_category_ids();
+                
+                if (in_array($item['product_id'], $selected_products) || array_intersect($product_categories, $selected_categories)) {
                     $product_name = sprintf('<a href="%s" target="_blank">%s</a>', get_permalink($item['product_id']), $item['data']->get_name());
                     $matching_products[] = $product_name;
                 }
@@ -167,7 +190,7 @@ if (!class_exists('Woo_Delivery_Display')) {
                 $final_message = sprintf($cart_reminder_message, $product_links);
                 wc_print_notice($final_message, 'notice');
             }
-        }
+        }        
         
     }
 
