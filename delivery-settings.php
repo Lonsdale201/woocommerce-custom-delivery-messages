c3d9339f412be3c35e79dbb1405c7f5863c580c6
Soczó <lonsdale201@hotmail.com>
2023-08-30 01:04:16 +0200

    Update delivery-settings.php v2.0

--- delivery-settings.php
+++ delivery-settings.php
@@ -17,8 +17,6 @@ if (!class_exists('Woo_Delivery_Notifications_Settings')) {
             // Admin oszlop és szűrő hozzáadása
             add_filter('manage_edit-product_columns', array($this, 'add_admin_column'));
             add_action('manage_product_posts_custom_column', array($this, 'display_admin_column_data'), 10, 2);
-            add_action('restrict_manage_posts', array($this, 'add_admin_filter_dropdown'));
-            add_filter('parse_query', array($this, 'filter_products_by_delivery_status'));
         }
         
 
@@ -33,20 +31,20 @@ if (!class_exists('Woo_Delivery_Notifications_Settings')) {
                 return $settings_tabs;  // Visszatérünk az eredeti $settings_tabs értékkel, ha nincs megfelelő jogosultság
             }
         
-            $settings_tabs['delivery_notifications'] = __('Delivery Notifications', 'woocommerce');
+            $settings_tabs['delivery_notifications'] = __('Delivery Notifications', 'woocommerce-delivery-notifications');
             return $settings_tabs;
         }
         // Beállítások hozzáadása az új fülhöz
         public function settings_tab() {
             if (!current_user_can('manage_options')) {
-                wp_die(__('You do not have sufficient permissions to access this page.'));
+                wp_die(__('You do not have sufficient permissions to access this page.', 'woocommerce-delivery-notifications'));
             }
             woocommerce_admin_fields($this->get_settings());
         }
         // Beállítások mentése
         public function update_settings() {
             if (!current_user_can('manage_options')) {
-                wp_die(__('You do not have sufficient permissions to update these settings.'));
+                wp_die(__('You do not have sufficient permissions to update these settings.', 'woocommerce-delivery-notifications'));
             }
             woocommerce_update_options($this->get_settings());
         }
@@ -67,154 +65,204 @@ if (!class_exists('Woo_Delivery_Notifications_Settings')) {
 
             return $product_options;
         }
+        // Termékkategóriák lekérdezése az adatbázisból
+        public function get_product_categories() {
+            $categories = get_terms( array(
+                'taxonomy' => 'product_cat',
+                'hide_empty' => false,
+            ) );
 
+            $category_options = array();
+
+            if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) {
+                foreach ( $categories as $category ) {
+                    $category_options[ $category->term_id ] = $category->name;
+                }
+            }
+
+            return $category_options;
+        }
+        // admin oszlop
         public function add_admin_column($columns) {
             $new_columns = array();
             foreach ($columns as $key => $value) {
                 $new_columns[$key] = $value;
                 if ($key == 'price') {
-                    $new_columns['delivery_status'] = __('Delivery Status', 'woocommerce');
+                    $new_columns['delivery_status'] = __('Delivery Status', 'woocommerce-delivery-notifications');
                 }
             }
             return $new_columns;
         }
-    
+
         public function display_admin_column_data($column, $post_id) {
             if ($column == 'delivery_status') {
                 $selected_products = get_option('wc_delivery_selected_products', array());
-                if (in_array($post_id, $selected_products)) {
-                    echo __('Extend delivery time', 'woocommerce');
+                $selected_categories = get_option('wc_delivery_selected_categories', array());
+                $product = wc_get_product($post_id);
+                $product_categories = $product->get_category_ids();
+                
+                if (in_array($post_id, $selected_products) || array_intersect($product_categories, $selected_categories)) {
+                    echo __('Extend delivery time', 'woocommerce-delivery-notifications');
                 }
             }
         }
-    
-        public function add_admin_filter_dropdown() {
-            global $typenow;
-    
-            if ($typenow == 'product') {
-                $selected = isset($_GET['delivery_status']) ? $_GET['delivery_status'] : '';
-                ?>
-                <select name="delivery_status">
-                    <option value=""><?php _e('Filter by Delivery Status', 'woocommerce'); ?></option>
-                    <option value="extended" <?php selected($selected, 'extended'); ?>><?php _e('Extended delivery time', 'woocommerce'); ?></option>
-                </select>
-                <?php
-            }
-        }
-    
-        public function filter_products_by_delivery_status($query) {
-            global $pagenow, $typenow;
-    
-            if ($pagenow == 'edit.php' && $typenow == 'product' && isset($_GET['delivery_status']) && $_GET['delivery_status'] == 'extended') {
-                $query->query_vars['post__in'] = get_option('wc_delivery_selected_products', array());
-            }
-    
-            return $query;
-        }
+     
         // A beállítások definiálása
         public function get_settings() {
             $settings = array(
                 'section_title' => array(
-                    'name'     => __('Delivery Notifications Settings', 'woocommerce'),
-                    'type'     => 'title',
-                    'desc'     => '',
-                    'id'       => 'wc_delivery_notifications_section_title'
+                    'name' => __('Delivery Notifications Settings', 'woocommerce-delivery-notifications'),
+                    'type' => 'title',
+                    'desc' => 'Show delivery informations for your customers. use the <code>[woo_delivery_notification]</code> if the placement set to shortcode',
+                    'id' => 'wc_delivery_notifications_section_title'
                 ),
                 'selected_products' => array(
-                    'name'     => __('Select Products', 'woocommerce'),
+                    'name' => __('Select Products', 'woocommerce-delivery-notifications'),
+                    'type' => 'multiselect',
+                    'class' => 'wc-enhanced-select',
+                    'desc_tip' => __('Select products for delivery notifications.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_selected_products',
+                    'options' => $this->get_products()  // Termékek lekérdezése
+                ),
+                'selected_categories' => array(
+                    'name'     => __('Select Product Categories', 'woocommerce-delivery-notifications'),
                     'type'     => 'multiselect',
                     'class'    => 'wc-enhanced-select',  
-                    'desc_tip' => __('Select products for delivery notifications.', 'woocommerce'),
-                    'id'       => 'wc_delivery_selected_products',
-                    'options'  => $this->get_products()  // Termékek lekérdezése
-                ),
-                'hide_if_out_of_stock' => array(
-                    'name'     => __('Hide if out of stock', 'woocommerce'),
-                    'type'     => 'checkbox',
-                    'desc_tip' => __('Hide the notification if the product is out of stock.', 'woocommerce'),
-                    'id'       => 'wc_delivery_hide_if_out_of_stock',
-                    'default'  => 'no'
-                ),       
-                'product_admin_column_delivery' => array(
-                    'name'     => __('Show Delivery Admin Column', 'woocommerce'),
-                    'type'     => 'checkbox',
-                    'desc_tip' => __('Enable to show the Delivery admin column.', 'woocommerce'),
-                    'id'       => 'wc_delivery_admin_column',
-                    'default'  => 'no'
-                ),             
-                'notification_placement' => array(
-                    'name'     => __('Notification Placement', 'woocommerce'),
-                    'type'     => 'select',
-                    'desc_tip' => __('Choose where the notification should appear.', 'woocommerce'),
-                    'id'       => 'wc_delivery_notification_placement',
-                    'default'  => 'below_add_to_cart',
-                    'options'  => array(
-                        'below_add_to_cart' => __('Below Add to Cart', 'woocommerce'),
-                        'shortcode' => __('Shortcode', 'woocommerce')
-                    )
+                    'desc_tip' => __('Select product categories for delivery notifications.', 'woocommerce-delivery-notifications'),
+                    'id'       => 'wc_delivery_selected_categories',
+                    'options'  => $this->get_product_categories()  // Termékkategóriák lekérdezése
                 ),                
+                'hide_if_out_of_stock' => array(
+                    'name' => __('Hide if out of stock', 'woocommerce-delivery-notifications'),
+                    'type' => 'checkbox',
+                    'desc_tip' => __('Hide the notification if the product is out of stock.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_hide_if_out_of_stock',
+                    'default' => 'no'
+                ),
+                'product_admin_column_delivery' => array(
+                    'name' => __('Show Delivery Admin Column', 'woocommerce-delivery-notifications'),
+                    'type' => 'checkbox',
+                    'desc_tip' => __('Enable to show the Delivery admin column.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_admin_column',
+                    'default' => 'no'
+                ),
+                'notification_placement' => array(
+                    'name' => __('Notification Placement', 'woocommerce-delivery-notifications'),
+                    'type' => 'select',
+                    'desc_tip' => __('Choose where the notification should appear.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_notification_placement',
+                    'default' => 'below_add_to_cart',
+                    'options' => array(
+                        'below_add_to_cart' => __('Below Add to Cart', 'woocommerce-delivery-notifications'),
+                        'above_add_to_cart' => __('Above Add to Cart', 'woocommerce-delivery-notifications'),
+                        'shortcode' => __('Shortcode', 'woocommerce-delivery-notifications')
+                    )
+                ),
                 'notification_title' => array(
-                    'name' => __('Notification Title', 'woocommerce'),
+                    'name' => __('Notification Title', 'woocommerce-delivery-notifications'),
                     'type' => 'text',
-                    'desc_tip' => __('Enter the title for the delivery notification.', 'woocommerce'),
-                    'id'   => 'wc_delivery_notification_title'
+                    'desc_tip' => __('Enter the title for the delivery notification.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_notification_title'
                 ),
                 'notification_message' => array(
-                    'name' => __('Notification Message', 'woocommerce'),
+                    'name' => __('Notification Message', 'woocommerce-delivery-notifications'),
                     'type' => 'textarea',
-                    'desc_tip' => __('Enter the long notification message for deliveries.', 'woocommerce'),
-                    'id'   => 'wc_delivery_notification_message',
-                    'css'  => 'min-width:300px; min-height:200px;'
+                    'desc_tip' => __('Enter the long notification message for deliveries.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_notification_message',
+                    'css' => 'min-width:300px; min-height:200px;'
                 ),
                 'cart_reminder' => array(
-                    'name' => __('Cart Reminder', 'woocommerce'),
+                    'name' => __('Cart Reminder', 'woocommerce-delivery-notifications'),
                     'type' => 'checkbox',
-                    'desc' => __('Display the notification on the cart page if applicable.', 'woocommerce'),
-                    'id'   => 'wc_delivery_cart_reminder',
+                    'desc' => __('Display the notification on the cart page if applicable.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_cart_reminder',
                     'default' => 'no'
-                ),               
+                ),
                 'cart_reminder_message' => array(
-                    'name' => __('Cart Reminder Message', 'woocommerce'),
+                    'name' => __('Cart Reminder Message', 'woocommerce-delivery-notifications'),
                     'type' => 'textarea',
-                    'desc_tip' => __('Enter the reminder message for the cart page. Use %s to display product names.', 'woocommerce'),
-                    'id'   => 'wc_delivery_cart_reminder_message',
-                    'css'  => 'min-width:300px; min-height:200px;'
-                ),                 
+                    'desc_tip' => __('Enter the reminder message for the cart page. Use %s to display product names.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_cart_reminder_message',
+                    'css' => 'min-width:300px; min-height:200px;'
+                ),
                 'section_end' => array(
                     'type' => 'sectionend',
                     'id' => 'wc_delivery_notifications_section_end'
                 ),
                 'regular_section_title' => array(
-                    'name' => __('Delivery notification settings for regular', 'woocommerce'),
+                    'name' => __('Delivery notification settings for regular', 'woocommerce-delivery-notifications'),
                     'type' => 'title',
                     'desc' => '',
-                    'id'   => 'wc_delivery_regular_section_title'
+                    'id' => 'wc_delivery_regular_section_title'
                 ),
                 'enable_regular_notification' => array(
-                    'name'     => __('Enable regular delivery notification', 'woocommerce'),
-                    'type'     => 'checkbox',
-                    'desc_tip' => __('Enable notification for products not in the selected list.', 'woocommerce'),
-                    'id'       => 'wc_delivery_enable_regular_notification',
-                    'default'  => 'no'
+                    'name' => __('Enable regular delivery notification', 'woocommerce-delivery-notifications'),
+                    'type' => 'checkbox',
+                    'desc_tip' => __('Enable notification for products not in the selected list.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_enable_regular_notification',
+                    'default' => 'no'
                 ),
                 'regular_notification_title' => array(
-                    'name' => __('Notification Title for Regular Products', 'woocommerce'),
+                    'name' => __('Notification Title for Regular Products', 'woocommerce-delivery-notifications'),
                     'type' => 'text',
-                    'desc_tip' => __('Enter the title for the delivery notification for regular products.', 'woocommerce'),
-                    'id'   => 'wc_delivery_regular_notification_title'
+                    'desc_tip' => __('Enter the title for the delivery notification for regular products.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_regular_notification_title'
                 ),
                 'regular_notification_message' => array(
-                    'name' => __('Notification Message for Regular Products', 'woocommerce'),
+                    'name' => __('Notification Message for Regular Products', 'woocommerce-delivery-notifications'),
                     'type' => 'textarea',
-                    'desc_tip' => __('Enter the long notification message for regular product deliveries.', 'woocommerce'),
-                    'id'   => 'wc_delivery_regular_notification_message',
-                    'css'  => 'min-width:300px; min-height:200px;'
+                    'desc_tip' => __('Enter the long notification message for regular product deliveries.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_regular_notification_message',
+                    'css' => 'min-width:300px; min-height:200px;'
                 ),
                 'regular_section_end' => array(
                     'type' => 'sectionend',
                     'id' => 'wc_delivery_regular_section_end'
-                )
-            );
+                ),
+                'delivery_product_tab_section_title' => array(
+                    'name' => __('Delivery product tab', 'woocommerce-delivery-notifications'),
+                    'type' => 'title',
+                    'desc' => __('Create a new Delivery Tab of every product to inform the customers about the shipping', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_product_tab_section_title'
+                ),
+                'enable_delivery_product_tab' => array(
+                    'name' => __('Enable Delivery Product tab', 'woocommerce-delivery-notifications'),
+                    'type' => 'checkbox',
+                    'desc' => '',
+                    'id' => 'wc_enable_delivery_product_tab',
+                    'default' => 'no'
+                ),
+                'delivery_tab_label' => array(
+                    'name' => __('Delivery tab Label', 'woocommerce-delivery-notifications'),
+                    'type' => 'text',
+                    'desc_tip' => __('Label for the Delivery tab on the product page.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_tab_label',
+                    'css' => 'min-width:300px;'
+                ),
+                'delivery_tab_priority' => array(
+                    'name' => __('Delivery Tab Priority', 'woocommerce-delivery-notifications'),
+                    'type' => 'number',
+                    'desc_tip' => __('Set the priority for the Delivery tab. Lower numbers correspond to higher priority and will display the tab earlier.', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_tab_priority',
+                    'default' => '50',  // alapértelmezett érték
+                    'custom_attributes' => array(
+                        'min' => '1',    // Min érték
+                        'step' => '1'    // Stepper
+                    )
+                ),
+                'delivery_tab_content' => array(
+                    'name' => __('Delivery tab content', 'woocommerce-delivery-notifications'),
+                    'type' => 'textarea',
+                    'desc' => 'Content for the Delivery tab on the product page. You can use SHORTCODE, or HTML.',
+                    'desc_tip' => __('Content for the Delivery tab on the product page. You can use SHORTCODE, or HTML. ', 'woocommerce-delivery-notifications'),
+                    'id' => 'wc_delivery_tab_content',
+                    'css' => 'min-width:300px; min-height:200px;'
+                ),
+                'delivery_product_tab_section_end' => array(
+                    'type' => 'sectionend',
+                    'id' => 'wc_delivery_product_tab_section_end'
+                )                
+            );            
 
             return apply_filters('wc_delivery_notifications_settings', $settings);
         }
